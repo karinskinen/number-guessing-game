cd5341884675a07f14113548b1cc478b3f714fa2
Karina Määttänen <karina.maattanen@gmail.com>
2024-02-22 16:03:38 +0100

    better game experience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import art
 import random as r
-
-game_continuation = True
+import os
 
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
@@ -12,7 +11,7 @@ def difficulty():
     return EASY_LEVEL_TURNS
   else:
     return HARD_LEVEL_TURNS
-def compare():
+def compare(chosen_number, guessed_number):
   if chosen_number == guessed_number:
     game_continuation = False
     print(f"Wow, great job! You've guessed my number {chosen_number} correctly")
@@ -20,32 +19,37 @@ def compare():
     print("The number you've chosen is to high. Guess again")
   elif chosen_number > guessed_number:
     print("The number you've chosen is to low. Guess again")
-    
-print(art.logo)
-print("Welcome to this numbers game where I, the computer, have chosen a number")
-print("between 1 and 100 and you, the gamer, need to guess it.")
 
-number = 0
-numbers = []
-for number in range(100):
-  number += 1
-  numbers.append(number)
-chosen_number = r.choice(numbers)
-turns = difficulty()
-while game_continuation:
-  for guess in range(turns):
-    guessed_number = int(input("Guess the number: "))
-    compare()
-    if chosen_number == guessed_number:
+def game():
+  print(art.logo)
+  print("Welcome to this numbers game where I, the computer, have chosen a number")
+  print("between 1 and 100 and you, the gamer, need to guess it.")
+  game_continuation = True
+  number = 0
+  numbers = []
+  for number in range(100):
+    number += 1
+    numbers.append(number)
+  chosen_number = r.choice(numbers)
+  turns = difficulty()
+  while game_continuation:
+    for guess in range(turns):
+      guessed_number = int(input("Guess the number: "))
+      compare(chosen_number, guessed_number)
+      if chosen_number == guessed_number:
+        game_continuation = False
+        break
+    if chosen_number != guessed_number: 
       game_continuation = False
-      break
-  if chosen_number != guessed_number: 
-    game_continuation = False
-    print(f"Your {turns} guesses are up! My number was {chosen_number}")
+      print(f"Your {turns} guesses are up! My number was {chosen_number}")
+  another_game = input("Do you want continue game, type yes or no ? ").lower()
+  if another_game == "yes":
+      os.system('clear')
+      game()
+  else:
+      print("Thank you for a nice game. See you another day!")
 
-
-
-
+game()
 
 
 
